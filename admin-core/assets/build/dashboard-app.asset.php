ba699694c0adf2dad0df9b7e087456c98c55cb77
Supriya Jadhav <supriyaj@bsf.io>
2023-02-27 15:52:56 +0530

    Resolved variation issues

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '2e59409d3286e0f3533995ac2185445c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'caf82ca6ed5f890368f6526817c90eee');
