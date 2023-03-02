ea3db0fc7793649e0a55554c349faf503e242127
Sai Chandan <saichandan@Sais-MacBook-Air.local>
2023-03-02 16:06:42 +0530

    resolved phpcs issues

--- includes/blocks/price-table/block.php
+++ includes/blocks/price-table/block.php
@@ -8,16 +8,16 @@
  */
 
 $block_slug = 'uagb/price-table';
-$block_data = [
-    'doc'              => 'price-table',
-    'slug'             => '',
-    'admin_categories' => [ 'content' ],
-    'link'             => 'price-table',
-    'title'            => __('Price Table', 'ultimate-addons-for-gutenberg'),
-    'description'      => __('Editable price table block', 'ultimate-addons-for-gutenberg'),
-    'default'          => true,
-    'extension'        => false,
-    'priority'         => Spectra_Block_Prioritization::get_block_priority('price-table'),
-    'deprecated'       => false,
-    'dynamic_assets'   => ['dir' => 'price-table'],
-];
+$block_data = array(
+	'doc'              => 'price-table',
+	'slug'             => '',
+	'admin_categories' => array( 'content' ),
+	'link'             => 'price-table',
+	'title'            => __( 'Price Table', 'ultimate-addons-for-gutenberg' ),
+	'description'      => __( 'Editable price table block', 'ultimate-addons-for-gutenberg' ),
+	'default'          => true,
+	'extension'        => false,
+	'priority'         => Spectra_Block_Prioritization::get_block_priority( 'price-table' ),
+	'deprecated'       => false,
+	'dynamic_assets'   => array( 'dir' => 'price-table' ),
+);
--- includes/blocks/price-table/frontend.css.php
+++ includes/blocks/price-table/frontend.css.php
@@ -8,52 +8,52 @@
  */
 
 $selectors    = array(
-    ' .box'        => array(
-        'text-align' => $attr['textAlign'],
-    ),
-    ' .box__title' => array(
-        'color'           => $attr['titleFontColor'],
-        'font-family'     => $attr['titleFontFamily'],
-        'font-size'       => UAGB_Helper::get_css_value($attr['titleFontSize'], $attr['titleFontSizeType']),
-        'font-weight'     => $attr['titleFontWeight'],
-        'font-style'      => $attr['titleFontStyle'],
-        'text-transform'  => $attr['titleTransform'],
-        'text-decoration' => $attr['titleDecoration'],
-        'line-height'     => UAGB_Helper::get_css_value($attr['titleLineHeight'], $attr['titleLineHeightType']),
-        'letter-spacing'  => UAGB_Helper::get_css_value($attr['titleLetterSpacing'], $attr['titleLetterSpacingType']),
-    ),
+	' .box'        => array(
+		'text-align' => $attr['textAlign'],
+	),
+	' .box__title' => array(
+		'color'           => $attr['titleFontColor'],
+		'font-family'     => $attr['titleFontFamily'],
+		'font-size'       => UAGB_Helper::get_css_value( $attr['titleFontSize'], $attr['titleFontSizeType'] ),
+		'font-weight'     => $attr['titleFontWeight'],
+		'font-style'      => $attr['titleFontStyle'],
+		'text-transform'  => $attr['titleTransform'],
+		'text-decoration' => $attr['titleDecoration'],
+		'line-height'     => UAGB_Helper::get_css_value( $attr['titleLineHeight'], $attr['titleLineHeightType'] ),
+		'letter-spacing'  => UAGB_Helper::get_css_value( $attr['titleLetterSpacing'], $attr['titleLetterSpacingType'] ),
+	),
 );
 $mobselectors = array(
-    ' .uagb-user__title' => array(
-        'text-align'      => $attr['textAlignTablet'],
-        'font-family'     => $attr['titleFontFamily'],
-        'font-size'       => UAGB_Helper::get_css_value($attr['titleFontSizeMobile'], $attr['titleFontSizeType']),
-        'font-weight'     => $attr['titleFontWeight'],
-        'font-style'      => $attr['titleFontStyle'],
-        'text-transform'  => $attr['titleTransform'],
-        'text-decoration' => $attr['titleDecoration'],
-        'line-height'     => UAGB_Helper::get_css_value($attr['titleLineHeightMobile'], $attr['titleLineHeightType']),
-        'letter-spacing'  => UAGB_Helper::get_css_value($attr['titleLetterSpacingMobile'], $attr['titleLetterSpacingType']),
-    ),
+	' .uagb-user__title' => array(
+		'text-align'      => $attr['textAlignTablet'],
+		'font-family'     => $attr['titleFontFamily'],
+		'font-size'       => UAGB_Helper::get_css_value( $attr['titleFontSizeMobile'], $attr['titleFontSizeType'] ),
+		'font-weight'     => $attr['titleFontWeight'],
+		'font-style'      => $attr['titleFontStyle'],
+		'text-transform'  => $attr['titleTransform'],
+		'text-decoration' => $attr['titleDecoration'],
+		'line-height'     => UAGB_Helper::get_css_value( $attr['titleLineHeightMobile'], $attr['titleLineHeightType'] ),
+		'letter-spacing'  => UAGB_Helper::get_css_value( $attr['titleLetterSpacingMobile'], $attr['titleLetterSpacingType'] ),
+	),
 );
 
 $tabselectors       = array(
-    ' .uagb-user__title' => array(
-        'text-align'      => $attr['textAlignMobile'],
-        'font-family'     => $attr['titleFontFamily'],
-        'font-size'       => UAGB_Helper::get_css_value($attr['titleFontSizeTablet'], $attr['titleFontSizeType']),
-        'font-weight'     => $attr['titleFontWeight'],
-        'font-style'      => $attr['titleFontStyle'],
-        'text-transform'  => $attr['titleTransform'],
-        'text-decoration' => $attr['titleDecoration'],
-        'line-height'     => UAGB_Helper::get_css_value($attr['titleLineHeightTablet'], $attr['titleLineHeightType']),
-        'letter-spacing'  => UAGB_Helper::get_css_value($attr['titleLetterSpacingTablet'], $attr['titleLetterSpacingType']),
-    ),
+	' .uagb-user__title' => array(
+		'text-align'      => $attr['textAlignMobile'],
+		'font-family'     => $attr['titleFontFamily'],
+		'font-size'       => UAGB_Helper::get_css_value( $attr['titleFontSizeTablet'], $attr['titleFontSizeType'] ),
+		'font-weight'     => $attr['titleFontWeight'],
+		'font-style'      => $attr['titleFontStyle'],
+		'text-transform'  => $attr['titleTransform'],
+		'text-decoration' => $attr['titleDecoration'],
+		'line-height'     => UAGB_Helper::get_css_value( $attr['titleLineHeightTablet'], $attr['titleLineHeightType'] ),
+		'letter-spacing'  => UAGB_Helper::get_css_value( $attr['titleLetterSpacingTablet'], $attr['titleLetterSpacingType'] ),
+	),
 );
 $combined_selectors = array(
-    'desktop' => $selectors,
-    'tablet'  => $tabselectors,
-    'mobile'  => $mobselectors,
+	'desktop' => $selectors,
+	'tablet'  => $tabselectors,
+	'mobile'  => $mobselectors,
 );
 $base_selector      = '.uagb-block-';
-return UAGB_Helper::generate_all_css($combined_selectors, $base_selector.$id);
+return UAGB_Helper::generate_all_css( $combined_selectors, $base_selector . $id );
