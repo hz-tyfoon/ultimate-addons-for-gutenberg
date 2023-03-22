a28f884b44c26804f4cc5096f63d3de0c3fc3d28
rahulvarma722 <rahulvarma722@gmail.com>
2023-03-22 14:18:39 +0530

    latest build

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-edit-post', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-keyboard-shortcuts', 'wp-plugins', 'wp-polyfill', 'wp-primitives', 'wp-rich-text', 'wp-url'), 'version' => '03a93fcb690e929e0b72b879cb7d8ac7');
-=======
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-edit-post', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-keyboard-shortcuts', 'wp-plugins', 'wp-polyfill', 'wp-primitives', 'wp-rich-text', 'wp-url'), 'version' => '03c8dfb348d2867981807129bcaac719');
->>>>>>> a092a8ad9654e3dc502ba4d289ce9789a4ed5e3f
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-edit-post', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-keyboard-shortcuts', 'wp-plugins', 'wp-polyfill', 'wp-primitives', 'wp-rich-text', 'wp-url'), 'version' => '632d68bd650b8728798936cc517caa3b');
