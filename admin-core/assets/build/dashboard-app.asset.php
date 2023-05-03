63ace305848f753120f34d6c2bc3f57e76020ac6
Aditya Jain <adi3890@gmail.com>
2023-05-04 02:18:03 +0530

    adding loop builder svgs

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n'), 'version' => '362c51efdf4d509fd74a98c9dcefa935');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n'), 'version' => '35fb7ae0bc235eba66ddcbc93f9243f0');
