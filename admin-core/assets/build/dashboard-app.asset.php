64fda7578181e161ff659411a64cbd54b54b20d1
Aditya Jain <adi3890@gmail.com>
2023-05-04 12:11:59 +0530

    fixing admin core loop builder icon

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n'), 'version' => '35fb7ae0bc235eba66ddcbc93f9243f0');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n'), 'version' => 'c5abc1cf42215ca868ab636d40c5716f');
