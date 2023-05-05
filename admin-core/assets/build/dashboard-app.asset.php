3e18adde8b40c019e2464906909e12cc2b4f64f2
Aditya Jain <adi3890@gmail.com>
2023-05-05 09:58:27 +0530

    SPCTR-2750 : Adding designer provides assets to loop builder (#3085)
    * adding loop builder svgs
    
    * fixing admin core loop builder icon
    
    * suggested changes

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n'), 'version' => '362c51efdf4d509fd74a98c9dcefa935');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n'), 'version' => 'c5abc1cf42215ca868ab636d40c5716f');
