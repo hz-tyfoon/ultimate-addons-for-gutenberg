583ba12581d0b66321af22c45272ff083314bfbe
Raj Mohan <68589327+rajmohanreddy774@users.noreply.github.com>
2023-05-08 20:59:38 +0530

    formating file

--- blocks-config/uagb-controls/addInitialAttr.js
+++ blocks-config/uagb-controls/addInitialAttr.js
@@ -1,39 +1,48 @@
 import { useEffect } from '@wordpress/element';
-const addInitialAttr = ( ChildComponent )=> {
+const addInitialAttr = ( ChildComponent ) => {
 	const WrappedComponent = ( props ) => {
-        const { name, setAttributes, clientId } = props;
+		const { name, setAttributes, clientId } = props;
 
 		useEffect( () => {
-            const listOfClassMigrate = [ 'uagb/advanced-heading', 'uagb/blockquote', 'uagb/buttons', 'uagb/call-to-action', 'uagb/column', 'uagb/columns', 'uagb/icon-list', 'uagb/image-gallery' ];
+			const listOfClassMigrate = [
+				'uagb/advanced-heading',
+				'uagb/blockquote',
+				'uagb/buttons',
+				'uagb/call-to-action',
+				'uagb/column',
+				'uagb/columns',
+				'uagb/icon-list',
+				'uagb/image-gallery',
+			];
 
-            const listOfChildMigrate = [ 'uagb/buttons', 'uagb/icon-list' ];
+			const listOfChildMigrate = [ 'uagb/buttons', 'uagb/icon-list' ];
 
-            const listOfIsHtml = [ 'uagb/cf7-styler', 'uagb/gf-styler' ];
+			const listOfIsHtml = [ 'uagb/cf7-styler', 'uagb/gf-styler' ];
 
-            const listOfEditorInnerblocksPreview = [ 'uagb/countdown' ]
-          
-            if ( listOfEditorInnerblocksPreview.includes( name ) ) {
-                // editorInnerblocksPreview: This attribute is used to display innerblocks preview for 'Replace with Content' mode.
-		        setAttributes( {
-			       editorInnerblocksPreview: false,
-		        } );
-            }
-            if( listOfIsHtml.includes( name ) ) {
-                setAttributes( { isHtml: false } );
-            }
-            if( listOfChildMigrate.includes( name ) ) {
-                setAttributes( { childMigrate: true } );
-            }
-            if( listOfClassMigrate.includes( name ) ) {
-                setAttributes( { classMigrate: true } );
-            }
+			const listOfEditorInnerblocksPreview = [ 'uagb/countdown' ];
 
-            setAttributes( { block_id: clientId.substr( 0, 8 ) } );
-        }, [] );
+			if ( listOfEditorInnerblocksPreview.includes( name ) ) {
+				// editorInnerblocksPreview: This attribute is used to display innerblocks preview for 'Replace with Content' mode.
+				setAttributes( {
+					editorInnerblocksPreview: false,
+				} );
+			}
+			if ( listOfIsHtml.includes( name ) ) {
+				setAttributes( { isHtml: false } );
+			}
+			if ( listOfChildMigrate.includes( name ) ) {
+				setAttributes( { childMigrate: true } );
+			}
+			if ( listOfClassMigrate.includes( name ) ) {
+				setAttributes( { classMigrate: true } );
+			}
 
-		return <ChildComponent { ...props } />
-	}
+			setAttributes( { block_id: clientId.substr( 0, 8 ) } );
+		}, [] );
 
-    return WrappedComponent;
-}
+		return <ChildComponent { ...props } />;
+	};
+
+	return WrappedComponent;
+};
 export default addInitialAttr;
