94cf7599760b767ff0f73748b5fc3e34e1a93224
rahulvarma722 <rahulvarma722@gmail.com>
2023-06-02 14:27:45 +0530

    Pr comments resolve

--- src/blocks/google-map/render.js
+++ src/blocks/google-map/render.js
@@ -22,11 +22,14 @@ const Render = ( props ) => {
 		attributes: { zoom, address, language, height },
 		deviceType
 	} = props;
-
-	const encoded_address = encodeURI( address );
-	const lang_par = language ? language : 'en';
 	
-	const url = `https://maps.google.com/maps?q=${ encoded_address }&z=${ getFallbackNumber( zoom, 'zoom', blockName ) }&hl=${ lang_par }&t=m&output=embed&iwloc=near`;
+	const url = 'https://maps.google.com/maps'
+	url.searchParams.set( 'q', encodeURI( address ) );
+	url.searchParams.set( 'z', getFallbackNumber( zoom, 'zoom', blockName ) );
+	url.searchParams.set( 'hl', language ? language : 'en' );
+	url.searchParams.set( 't', 'm' );
+	url.searchParams.set( 'output', 'embed' );
+	url.searchParams.set( 'iwloc', 'near' );
 	
 	return (
 		<div
