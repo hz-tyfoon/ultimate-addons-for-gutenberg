34598deda6c966777cf666570be3804eea1d74ff
Antariksh Patre <antarikshpatre@gmail.com>
2023-06-22 16:30:47 +0530

    SPCTR-2786 Created Dynamic Content Trim Helper Function JS

--- blocks-config/utils/Helpers.js
+++ blocks-config/utils/Helpers.js
@@ -38,3 +38,61 @@ export function getPanelIdFromRef( ref ) {
 export const uagbClassNames = ( classes ) => classes.filter( Boolean ).join( ' ' );
 
 export const uagbDeepClone = ( arrayOrObject ) => JSON.parse( JSON.stringify( arrayOrObject ) );
+
+// Trim text by max character length to the last complete word. 
+export const trimTextToFullyVisibleWord = ( text, maxLength ) => {
+
+	if( ! text ) {
+		return '';
+	}
+
+	let needsEllipsis = true;
+	let limitedCaption = text;
+
+	if ( needsEllipsis && text.length <= maxLength ) {
+		// The caption is already below the limiter.
+		needsEllipsis = false;
+	} else if ( needsEllipsis ) {
+		limitedCaption = limitedCaption.substr( 0, maxLength );
+		if ( -1 === limitedCaption.lastIndexOf( ' ' ) ) {
+			// There's only 1 word.
+			if ( -1 === text.lastIndexOf( ' ' ) ) {
+				// There's only 1 word in the original caption.
+				if ( limitedCaption.length === text.split( ' ' )[ 0 ].length ) {
+					// The limited caption is the same as the original.
+					needsEllipsis = false;
+				} else {
+					// The limited caption differs from the original.
+					limitedCaption = '';
+				}
+			} else if ( limitedCaption.length !== text.split( ' ' )[ 0 ].length ) {
+				// There's more than 1 word in the original caption and...
+				// The limited caption is smaller than 1 word in the original.
+				limitedCaption = '';
+			}
+		} else if ( limitedCaption.length === text.length ) {
+			// There is a space and...
+			// The limited caption is the same as the original.
+			needsEllipsis = false;
+		} else if ( ' ' !== text.charAt( limitedCaption.length ) ) {
+			// The limited caption differs from the original and...
+			// The end of the limited text is not a word.
+			limitedCaption = limitedCaption.substr(
+				0,
+				Math.min( limitedCaption.length, limitedCaption.lastIndexOf( ' ' ) )
+			);
+		}
+	}
+
+	// Remove Trailing Characters.
+	switch ( limitedCaption.charAt( limitedCaption.length - 1 ) ) {
+		case ',':
+		case '.':
+		case ' ':
+		case "'":
+			limitedCaption = limitedCaption.slice( 0, limitedCaption.length - 1 );
+			break;
+	}
+
+	return `${ limitedCaption }${ needsEllipsis ? '…' : '' }`;
+};
