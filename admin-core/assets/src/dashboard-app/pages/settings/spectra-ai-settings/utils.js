a6eaf1dc9229e4d1ec27d379988906cfe25ca518
rahulvarma722 <rahulvarma722@gmail.com>
2023-08-23 21:06:41 +0530

    Change api url

--- admin-core/assets/src/dashboard-app/pages/settings/spectra-ai-settings/utils.js
+++ admin-core/assets/src/dashboard-app/pages/settings/spectra-ai-settings/utils.js
@@ -9,17 +9,17 @@ export const svgSpinner = (
 export async function authenticateApiKey( openAiKey ) {
 	const messageArray = [{ role: 'user', content: 'Spectra Testing' }];
 
-	const response = await fetch( 'https://api.openai.com/v1/usage', {
-	method: 'POST',
-	headers: {
-		'Content-Type': 'application/json',
-		'Authorization': `Bearer ${openAiKey}`, // will get data saved by user.
-	},
-	body: JSON.stringify( {
-		model: 'gpt-3.5-turbo', // we will provide option to choose this model later. for now keeping it as default.
-		messages: messageArray,
-	} ),
-	} );
+	const response = await fetch( 'https://api.openai.com/v1/chat/completions', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': `Bearer ${openAiKey}`, // will get data saved by user.
+			},
+			body: JSON.stringify( {
+				model: 'gpt-3.5-turbo', // we will provide option to choose this model later. for now keeping it as default.
+				messages: messageArray,
+			} ),
+		} );
 
 	return await response.json();
 }
