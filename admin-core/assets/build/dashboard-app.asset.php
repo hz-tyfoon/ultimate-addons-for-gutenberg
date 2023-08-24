16e915ad09c9a260b5616c29ede35a49934be0c7
Aditya Jain <adityajain@Adityas-MacBook-Air.local>
2023-08-24 21:56:19 +0530

    updating admin core

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-escape-html', 'wp-i18n'), 'version' => '68728cf142b0fd108188');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-escape-html', 'wp-i18n'), 'version' => 'cf212d86ba4e5942e840');
