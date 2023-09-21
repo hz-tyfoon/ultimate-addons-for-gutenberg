20c317d4faadd950406be838aa473192d1d38307
Aditya Jain <adityajain@Adityas-MacBook-Air.local>
2023-09-21 17:28:44 +0530

    sending classes in array

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n'), 'version' => '4b98a1c0c80b383b76c7');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n'), 'version' => '4251f6f7a5ac31b9f158');
--- admin-core/assets/src/dashboard-app/pages/blocks/AiExtension.js
+++ admin-core/assets/src/dashboard-app/pages/blocks/AiExtension.js
@@ -51,12 +51,12 @@ const AiExtension = () => {
     return (
         <div
         key={'ai'}
-        className={ uagbClassNames(
+        className={ uagbClassNames( [
             ( aiStatus && uag_react.spectra_pro_status )
                 ? 'border-white bg-white shadow hover:shadow-hover hover:z-50'
                 : 'border-slate-200 spectra-disabled-icon',
-            'box-border relative border rounded-md h-20 p-4 flex items-center gap-x-4 snap-start transition spectra-icon-transition'
-        ) }
+            'box-border relative border rounded-md h-20 p-4 flex items-center gap-x-4 snap-start transition spectra-icon-transition',
+		] ) }
         >
             <div className="flex-shrink-0">
                 { UAGB_Block_Icons.artificial_intelligence }
@@ -83,17 +83,17 @@ const AiExtension = () => {
                 <Switch
                     checked={aiStatus}
                     onChange={AiStatus}
-                    className={uagbClassNames(
+                    className={uagbClassNames( [
                         aiStatus ? 'bg-spectra' : 'bg-slate-200',
-                        'relative inline-flex flex-shrink-0 h-5 w-[2.4rem] items-center border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none'
-                    )}
+                        'relative inline-flex flex-shrink-0 h-5 w-[2.4rem] items-center border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none',
+					] )}
                     >
                     <span
                         aria-hidden="true"
-                        className={uagbClassNames(
+                        className={uagbClassNames( [
                         aiStatus ? 'translate-x-5' : 'translate-x-0',
-                        'pointer-events-none inline-block h-3.5 w-3.5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200'
-                        )}
+                        'pointer-events-none inline-block h-3.5 w-3.5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200',
+						] )}
                     />
                 </Switch>
             ) : (
