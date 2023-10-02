8d9ec00bfbed76511faefa66ca4b1d8013cf5b0b
Zane Vaz <zanev@bsf.io>
2023-10-02 09:39:02 +0530

    SPCTR-3396: Removed Unrequired Icon

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n'), 'version' => '0a9e1d8d3b481a072fa9');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n'), 'version' => '1e72e262be65a99796ac');
