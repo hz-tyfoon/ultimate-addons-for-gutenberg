da677ccd2719b2fc192bcad23fd6f8fd879ec4c7
Aditya Jain <adityajain@Adityas-MacBook-Air.local>
2023-10-05 15:51:52 +0530

    removing open ai key instance

--- admin-core/ajax/common-settings.php
+++ admin-core/ajax/common-settings.php
@@ -13,7 +13,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 use UagAdmin\Ajax\Ajax_Base;
-use UagAdmin\Inc\Admin_Helper;
 
 /**
  * Class Common_Settings.
@@ -802,11 +801,6 @@ class Common_Settings extends Ajax_Base {
 		$value = $this->check_post_value();
 		$value = json_decode( stripslashes( $value ), true );
 		$value = is_array( $value ) ? $value : array();
-
-		// Encrypt ai key.
-		if ( ! empty( $value['key'] ) ) {
-			$value['key'] = \UAGB_Admin_Helper::encrypt( $value['key'] );
-		}
 
 		// The previous $value is not sanitized, as the array sanitization is handled in the class method used below.
 		$this->save_admin_settings( 'uag_open_ai_options', $this->sanitize_form_inputs( $value ) );
