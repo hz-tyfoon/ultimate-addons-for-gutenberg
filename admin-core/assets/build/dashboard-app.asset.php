faa0b644e5e8efb399333861f64deee8cab98d17
rahulvarma722 <rahulvarma722@gmail.com>
2023-10-25 13:00:54 +0530

    Relocate beta text

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n'), 'version' => '16a501225292b854ad73');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n'), 'version' => 'd7775b3725296fe28037');
