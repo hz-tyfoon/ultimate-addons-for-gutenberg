52b44893dd1f749c48fa7afafefc66dd30ba124f
Zane Vaz <zanev@bsf.io>
2023-10-26 16:33:51 +0530

    SPCTR-3458: Removed Remnants of Brand Voice from the Dashboard

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n'), 'version' => 'b41dd85c77b51039e147');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n'), 'version' => '7fa94a850d2ac3412c50');
