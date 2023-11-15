4459d42d37826ed94b8c472c4bee6c1079cb52b4
mohitbsftester <mohits@bsf.io>
2023-11-15 13:21:07 +0530

    New update of Playwright, and New basic test case

--- e2e/my_second_test.spec.ts
+++ e2e/my_second_test.spec.ts
@@ -1,11 +1,36 @@
 import { test, expect } from '@playwright/test';
 
 
-test( 'first', async ( { page } ) => {
-    await page.goto( 'http://localhost:8888/wp-admin/options-general.php?page=spectra' );
+// test( 'first', async ( { page } ) => {
+//     await page.goto( 'http://spectra.test' );
   
-    const spectrax = page.locator( 'text=Welcome to Spectra!' );
+//     const spectrax = page.locator( 'text=A commitment to innovation and sustainability!' );
   
-    await expect( spectrax ).toContainText( 'Welcome to Spectra!' )
+//     await expect( spectrax ).toContainText( "" )
     
-  } );
+//   } );
+
+
+  test('has title', async ({ page }) => {
+    await page.goto('https://notable-normandeau-gfbwl.zipwp.dev/wp-admin/?wtlwp_token=6d8fb037b9d4f7aee9c35f3d5c3ab73088e60baf815f1e1859524931b16d65434b9037aa54f7f2ec7915da9f48101456eb42f317e66b151ee439a1a65634456b');
+  
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Dashboard/);
+  });
+  
+  test('docs', async ({ page }) => {
+    await page.goto('https://notable-normandeau-gfbwl.zipwp.dev/wp-admin/?wtlwp_token=6d8fb037b9d4f7aee9c35f3d5c3ab73088e60baf815f1e1859524931b16d65434b9037aa54f7f2ec7915da9f48101456eb42f317e66b151ee439a1a65634456b');
+  
+    // Click the get started link.
+    await page.getByRole('link', { name: 'Spectra' }).click();
+  
+   await expect (page.getByRole('link', { name: 'Get Support' })).toBeVisible();
+
+   
+
+  
+  //  await expect (page.getByRole('link', { name: 'Need Support?' })).toBeVisible();
+  
+    // Expects page to have a heading with the name of Installation.
+    //await expect(page.getByRole('heading', { name: 'Need Support?' })).toBeVisible();
+  });
