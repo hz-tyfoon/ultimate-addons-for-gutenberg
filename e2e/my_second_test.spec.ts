05df3127961b70698bd43665c3404f35d76f7e30
mohitbsftester <mohits@bsf.io>
2023-11-15 17:27:19 +0530

    New test cases with no duplicate

--- e2e/my_second_test.spec.ts
+++ e2e/my_second_test.spec.ts
@@ -1,14 +1,14 @@
-import { test, expect } from '@playwright/test';
+// import { test, expect } from '@playwright/test';
 
 
-// test( 'first', async ( { page } ) => {
-//     await page.goto( 'http://localhost:8888/wp-admin/options-general.php?page=spectra' );
+// // test( 'first', async ( { page } ) => {
+// //     await page.goto( 'http://localhost:8888/wp-admin/options-general.php?page=spectra' );
   
-//     const spectrax = page.locator( 'text=Welcome to Spectra!' );
+// //     const spectrax = page.locator( 'text=Welcome to Spectra!' );
   
-//     await expect( spectrax ).toContainText( 'Welcome to Spectra!' )
+// //     await expect( spectrax ).toContainText( 'Welcome to Spectra!' )
     
-//   } );
+// //   } );
 
   import { test, expect } from '@playwright/test';
 
