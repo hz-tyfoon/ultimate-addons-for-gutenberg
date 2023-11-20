35854368939efa9c22213ed250b0ae11c4918bfa
Zane Vaz <zanev@bsf.io>
2023-11-20 15:56:29 +0530

    SPCTR-3533: Updated the library with the removed dashboard text on the auth screen.

--- lib/zip-ai/admin/dashboard-app/build/dashboard-app.asset.php
+++ lib/zip-ai/admin/dashboard-app/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'b5ab518586233f9fd36c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '4ef37a31348ba18a5660');
